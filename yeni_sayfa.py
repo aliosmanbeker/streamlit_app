1eb018c04aef3a96d1f55228965dba66da26dcaf
Ali Osman BEKER <ali.beker@ftechalbs.com>
2024-07-25 15:42:50 +0300

    tablo boyutları ayarlandı2

--- yeni_sayfa.py
+++ yeni_sayfa.py
@@ -23,6 +23,6 @@ def show_page():
         file_path = f"datas/{selected_file}"
         try:
             df = pd.read_excel(file_path)
-            st.dataframe(df, height=800, width=1200)
+            st.dataframe(df, height=800, width=st.beta_container().width * 1.2)
         except Exception as e:
             st.error(f"Dosya yüklenirken bir hata oluştu: {e}")
